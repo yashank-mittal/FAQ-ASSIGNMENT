04e8588051f960a756fc7ee8fc2ba5d899e90ef6
yashank <yaashank.mittal@gmail.com>
2021-10-28 02:22:40 +0530

    change

--- Assignment/index.js
+++ Assignment/index.js
@@ -29,7 +29,7 @@ mongoose.connect(process.env.MONGO_URL,
 
 // seedDB();
 
-app.use(faqroutes);
+app.use('/faq',faqroutes);
 
 app.listen(process.env.PORT || 8080,()=>{
     console.log("Server is Running")
